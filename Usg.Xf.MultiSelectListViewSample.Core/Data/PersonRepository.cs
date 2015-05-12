373aaecfd5b3f2e4019fe00e2e1a06eb22dd47d7
Derek Winnicki <winnicki.derek@gmail.com>
2015-05-12 13:58:56 -0700

    Minor edits

--- Usg.Xf.MultiSelectListViewSample.Core/Data/PersonRepository.cs
+++ Usg.Xf.MultiSelectListViewSample.Core/Data/PersonRepository.cs
@@ -13,14 +13,14 @@ namespace Usg.Xf.MultiSelectListViewSample.Core
 				Name = "Troy", 
 			 	Address = "3 Main St.", 
 			    Age = 28, 
-				UriProfileImage = new UriImageSource { Uri = new Uri("http://upload.wikimedia.org/wikipedia/commons/5/55/Tamarin_portrait.JPG") } 
+				UriProfileImage = new UriImageSource { Uri = new Uri("https://lh4.googleusercontent.com/-LOOxOwg1EjA/AAAAAAAAAAI/AAAAAAAAAKI/kglBi6wV0KQ/photo.jpg?sz=100") } 
 			},
 			new Person 
 			{
 				Name = "Derek", 
 				Address = "37 Broadway Ave.", 
 				Age = 27, 
-				UriProfileImage = new UriImageSource { Uri = new Uri("http://upload.wikimedia.org/wikipedia/commons/5/55/Tamarin_portrait.JPG") }
+				UriProfileImage = new UriImageSource { Uri = new Uri("https://graph.facebook.com/1555193771368874/picture?type=normal") }
 			},
 			new Person 
 			{
